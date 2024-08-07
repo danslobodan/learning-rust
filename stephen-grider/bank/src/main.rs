1322d5ddde1522d78471f7f60d1854f8b8863d4a
Slobodan Dan <danastroman@gmail.com>
2024-08-07 12:16:58 +0200

    Lifetimes.

--- stephen-grider/bank/src/main.rs
+++ stephen-grider/bank/src/main.rs
@@ -13,6 +13,20 @@ impl Account {
             balance: 0,
         }
     }
+
+    fn summary(&self) -> String {
+        format!("{} has the balance of {}", self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -24,16 +38,35 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
-}
 
-fn print_account(account: Account) {
-    println!("{:#?}", account);
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts
+            .iter()
+            .map(|account| account.balance)
+            .sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
-    let bank = Bank::new();
-    let account = Account::new(1, String::from("me"));
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("Slobodan"));
 
-    print_account(account);
-    print_account(account);
+    account.deposit(500);
+    account.withdraw(200);
+
+    bank.add_account(account);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
