7645584f031522ab215796c7a168cf9d7ed6996d
Slobodan Dan <danastroman@gmail.com>
2024-08-20 13:06:46 +0200

    Errors and Results.

--- stephen-grider/logs/src/main.rs
+++ stephen-grider/logs/src/main.rs
@@ -1,8 +1,44 @@
 use std::fs;
-use std::error::Error;
+use std::io::Error;
 
-fn main() {
-    let text = fs::read_to_string("logs.txt");
+fn extract_errors(text: &str) -> Vec<String> {
+    let split_text = text.split("\n");
+    let mut results = vec![];
 
-    println!("{:#?}", text);
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
+        }
+    }
+
+    results
+}
+
+fn main() -> Result<(), Error> {
+    let text = fs::read_to_string("logs.txt")?;
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n"))?;
+
+    Ok(())
+
+    // let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    // let error_logs = extract_errors(text.as_str());
+    // fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text_that_was_read) => {
+    //         let error_logs = extract_errors(text_that_was_read.as_str());
+    //         println!("{:#?}", error_logs);
+
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => { println!("Wrote errors.txt") }
+    //             Err(reason_write_failed) => {
+    //                 println!("Writing of errors.txt failed: {}", reason_write_failed);
+    //             }
+    //         }
+    //     }
+    //     Err(why_this_failed) => {
+    //         println!("Failed to read file: {}", why_this_failed);
+    //     }
+    // }
 }
