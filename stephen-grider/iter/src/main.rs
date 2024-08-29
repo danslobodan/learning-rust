e493ec186937a46f175548bd69fd465ee0ef0210
Slobodan Dan <danastroman@gmail.com>
2024-08-29 11:08:44 +0200

    Lifetimes.

--- stephen-grider/iter/src/main.rs
+++ stephen-grider/iter/src/main.rs
@@ -1,65 +1,25 @@
-fn print_elements(elements: &[String]) {
-    // for element in elements {
-    //     println!("{}", element);
-    // }
-
-    elements
-        .iter()
-        // .map(|el| format!("{} {}", el, el))
-        .for_each(|el| println!("{}", el));
-}
-
-fn shorten_strings(elements: &mut [String]) {
-    elements.iter_mut().for_each(|el| el.truncate(1));
-}
-
-fn to_uppercase(elements: &mut [String]) -> Vec<String> {
-    elements
-        .iter()
-        .map(|el| el.to_uppercase())
-        .collect()
-}
-
-fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
-    vec_a.into_iter().for_each(|el| vec_b.push(el))
-}
-
-fn explode(elements: &[String]) -> Vec<Vec<String>> {
-    elements
-        .iter()
-        .map(|el|
-            el
-                .chars()
-                .map(|ch| ch.to_string())
-                .collect()
-        )
-        .collect()
-}
-
-fn find_color_or(elements: &[String], search: &str, fallback: &str) -> String {
-    elements
-        .iter()
-        .find(|el| el.contains(search))
-        .map_or(String::from(fallback), |el| el.to_string())
+#[derive(Debug)]
+struct Account {
+    balance: i32,
 }
 
 fn main() {
-    let mut colors = vec![String::from("red"), String::from("green"), String::from("blue")];
+    let accounts: Vec<Account> = vec![
+        Account { balance: 0 },
+        Account { balance: 10 },
+        Account { balance: -15 },
+        Account { balance: 27 },
+        Account { balance: -3 }
+    ];
 
-    // print_elements(&colors);
+    // TODO: Add in a call to the 'filter' iterator adaptor.
+    // Find accounts that have a balance less than 0.
+    // You can find documentation on 'filter' here:
+    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.filter
+    let negative_accounts = accounts
+        .iter()
+        .filter(|ac| ac.balance < 0)
+        .collect::<Vec<_>>();
 
-    // shorten_strings(&mut colors[1..3]);
-
-    // let uppercased = to_uppercase(&mut colors);
-
-    // let mut destination = vec![];
-    // move_elements(colors, &mut destination);
-
-    // let exploded = explode(&colors);
-    // println!("{:#?}", exploded);
-
-    let found_color = find_color_or(&colors, "re", "Orange");
-    println!("{:#?}", found_color);
-
-    // print_elements(&destination);
+    println!("Accounts with negative balance: {:#?}", negative_accounts);
 }
